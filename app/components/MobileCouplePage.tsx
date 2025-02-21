055c15658c79961bd47f7c5914969d37ba88621d
alessandrocruz5 <alessandrorafaelcruz@gmail.com>
2025-02-21 23:51:10 +0800

    Scroll and format

--- app/components/MobileCouplePage.tsx
+++ app/components/MobileCouplePage.tsx
@@ -57,7 +57,7 @@ function MobileCouplePage() {
             alt="faith"
             className="scale-[2] object-cover transform translate-x-[-5.5rem] brightness-[.6]"
           />
-          <div className="absolute flex flex-col text-latte font-bold text-center top-[4%] p-10 space-y-5">
+          <div className="absolute flex flex-col text-latte font-bold text-center top-[10%] p-10 space-y-5">
             <div className="text-3xl">A love rooted in faith</div>
             <div className="text-md space-y-3">
               <div>
